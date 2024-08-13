3a5fd2dfa9051e7333eccd3b7b6ab735f689c5ea
mat0606 <mat0606@hotmail.com>
2024-08-13 05:57:07 -0500

    change api for object

--- 05_create_bucket.sh
+++ 05_create_bucket.sh
@@ -2,6 +2,6 @@ echo "Create Bucket"
 calm update cache
 calm compile bp --file create-bucket/blueprint.py
 calm create bp --file create-bucket/blueprint.py --name create-bucket --force
-calm launch bp "create-bucket" --app_name create-bucket -p "Default" --environment OTC
+calm launch bp "create-bucket" --app_name create-bucket2 -p "Default" --environment OTC
 calm publish bp -v "1.0" -n "Create Objects Bucket" -w -pm -aa -p "BP Design Project" -c "BI-Productivity" "create-bucket"
 calm launch marketplace item -a "Create Objects Bucket" -pj "BP Design Project" -e "OTC" -p "Default" "Create Objects Bucket" 
--- create-bucket/scripts/Package_Package1_Action___install___Task_AssignUserstoBucket.py
+++ create-bucket/scripts/Package_Package1_Action___install___Task_AssignUserstoBucket.py
@@ -7,40 +7,44 @@ headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 user_list = []
 users = "@@{object_users}@@"
 selected_users = users.split(",")
-print selected_users
+print (selected_users)
 
-def getUserJSON(user_name):
+def getUserJSON(user_name, sid):
   user = {
-    "username": user_name,
-    "permissions": [
-        "READ",
-        "WRITE"
-      ]
+    "Action": "s3:*",
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::bucket",
+      "Principal": {
+        "AWS": [
+          user_name
+        ]
+      },
+      "Resource": "arn:aws:s3:::bucket",
+      "Sid": sid
   }
   return user
   
 for selected_user in selected_users:
-    user_list.append(getUserJSON(selected_user))
-    print "Selected User: {}".format(selected_user)
+    # hardcode the sid for adminisrator now.  Need time to retrieve the owner statement
+    user_list.append(getUserJSON(selected_user,"Owner_Statement_1723538721825"))
+    print ("Selected User: " + selected_user)
+
+print (user_list)
   
-
 payload = {
-  "name": "@@{bucket_name}@@",
-  "bucket_permissions": user_list
+  "Statement": user_list,
+  "Version": "2.0"
 }
-
-
 print (payload)
 # Set the url and make the call
-url = "https://" + ip + ":9440/oss/api/nutanix/v3/objectstores/@@{object_store_uuid}@@/buckets/@@{bucket_name}@@/share"
-resp = urlreq(url, verb='PUT', auth='BASIC', user=user, passwd=password, params=json.dumps(payload), headers=headers, verify=False)
+url = "https://" + ip + ":9440/oss/api/nutanix/v3/objectstore_proxy/@@{object_store_uuid}@@/buckets/@@{bucket_name}@@/policy"
+# Masked off temporary.
+#resp = urlreq(url, verb='PUT', auth='BASIC', user=user, passwd=password, params=json.dumps(payload), headers=headers, verify=False)
 
 # If the call went through successfully
-if resp.ok:
-  print("User @@{object_users}@@ added to bucket '@@{bucket_name}@@' successfully.")
-
-# If the call failed
-else:
-  print(url + " call failed.")
-  print(resp)
-  exit(1)
+#if resp.ok:
+#  print("User @@{object_users}@@ added to bucket '@@{bucket_name}@@' successfully.")
+#else:
+#  print(url + " call failed.")
+#  print(resp.text)
+#  exit(1)
