44bab57b6f523ba245fbf69299258677b5d5b4a2
mat0606 <mat0606@hotmail.com>
2024-12-31 05:36:55 -0600

    add in runbook to list vm metric using v4 api

---
+++ rb_list_vm_metric_v4/scripts/_Runbook_List_VM_Metric_v4_Task_GetVMStat.py
@@ -0,0 +1,33 @@
+
+# VARIABLE DECLARATION
+TENANT_TAG = "@@{tenant_tag}@@"
+minimum_priority = "@@{minimum_priority}@@"
+
+user = "@@{CRED_PC.username}@@"
+password = "@@{CRED_PC.secret}@@"
+ip = "10.42.155.39"
+
+#print ("@@{vpc}@@")
+payload = {
+  "filter": "name==@@{vm}@@"
+}
+
+url = "https://" + ip + ":9440/api/vmm/v4.0/ahv/config/vms"
+headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+url_method = "GET"
+r = urlreq(url, url_method, auth="BASIC", user=user, passwd=password, params=json.dumps(payload), verify=False, headers=headers)
+#print ("Status code: " + r.status_code)
+print("REST elapsed total time: " + str(r.elapsed.total_seconds()))  
+print ("Output: " + r.text)
+if r.ok:
+  vm_json = r.json()
+  vm = vm_json["data"][0]
+  vmExtId = vm["extId"]
+  print ("vmExtId: " + vmExtId)
+  
+url2 = "https:/" + ip + ":9440/api/vmm/v4.0/ahv/stats/vms/" + vmExtId + "?$startTime=@@{startTime}@@&$statType=@@{statType}@@&$endTime=@@{endTime}@@&$samplingInterval=1&$select=@@{queryParam}@@"
+r2 = urlreq(url, url_method, auth="BASIC", user=user, passwd=password, params=json.dumps(payload), verify=False, headers=headers)
+print("REST elapsed total time: " + str(r2.elapsed.total_seconds()))  
+print ("Output: " + r2.text)
+
+ 
---
+++ rb_list_vm_metric_v4/scripts/_Runbook_List_VM_Metric_v4_variable_vm_Task_SampleTask.py
@@ -0,0 +1,22 @@
+user = "@@{CRED_PC.username}@@"
+password = "@@{CRED_PC.secret}@@"
+ip = "10.42.155.39"
+
+
+payload = {
+  
+}
+
+url = "https://" + ip + ":9440/api/vmm/v4.0/ahv/config/vms?$limit=100"
+headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+url_method = "GET"
+r = urlreq(url, url_method, auth="BASIC", user=user, passwd=password, params=json.dumps(payload), verify=False, headers=headers)
+
+#print("REST elapsed total time: " + str(r.elapsed.total_seconds()))  
+#print ("Output: " + r.text)
+vm_list = []
+if r.ok:
+  vm_json = r.json()
+  for vm in vm_json['data']:
+    vm_list.append(vm['name'])
+print (','.join(vm_list))    
